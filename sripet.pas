ac035487cb49d35af7e1ccb9c4bb18e8be4519f6
syebil <168447494+syebil@users.noreply.github.com>
2024-04-30 02:18:52 +1200

    Add files via upload

---
+++ sripet.pas
@@ -0,0 +1,35 @@
+const wrapper document.querySelector(".wrapper");
+
+const question document.querySelector(".question");
+
+const gif document.querySelector(".gif");
+
+const yesBtn = document.querySelector(".yes-btn");
+
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () {
+
+question.innerHTML = "Aaaaa, I like you too";
+
+gif.src =
+
+"https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp"; });
+
+noBtn.addEventListener("mouseover", () => {
+
+const noBtnRect = noBtn.getBoundingClientRect();
+
+const maxX = window.innerWidthnoBtnRect.width;
+
+const maxy = window.innerHeight noBtnRect.height;
+
+const randomX = Math.floor(Math.random() maxx);
+
+const randomy = Math.floor(Math.random() maxy);
+
+noBtn.style.left = randomX + "px";
+
+noBtn.style.top randomy + "px";
+
+});
